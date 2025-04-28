472d38c47aa5bb554a1ff2e440e4eca4f8b77eac
naviteq-denys <denys@zouz.io>
2025-04-28 17:45:17 +0400

    feat(infra): fix outputs

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@ output "service_account_email" {
 
 output "account_id" {
   description = "The ID of the GCP account."
-  value       = google_service_account.this.account_id
+  value       = data.google_project.current.project_id
 }
 
 output "billing_export_table" {
@@ -16,7 +16,7 @@ output "billing_export_table" {
 output "billing_info" {
   description = "The billing export information."
   value = {
-    billing_export_table      = "${google_service_account.this.account_id}.${var.billing_export_dataset_name}.${var.billing_export_table_name}"
+    billing_export_table      = "${data.google_project.current.project_id}.${var.billing_export_dataset_name}.${var.billing_export_table_name}"
     billing_export_email      = google_service_account.this.email
     billing_export_project_id = google_service_account.this.project
   }
