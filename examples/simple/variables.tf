8bf9200170b732d48ee2fcf123af38089d5e4cc7
Denys <denys@zouz.io>
2025-10-23 17:04:09 +0400

    GCP Module Update (#4)
    Co-authored-by: Attribute <devops@attrb.io>

--- examples/simple/variables.tf
+++ examples/simple/variables.tf
@@ -11,9 +11,31 @@ variable "token" {
 variable "billing_export_dataset_name" {
   type        = string
   description = "The Billing Export dataset name."
+  default     = ""
 }
 
 variable "billing_export_table_name" {
   type        = string
   description = "The Billing Export table name."
+  default     = ""
+}
+
+variable "account_type" {
+  type        = string
+  description = "Account type to create"
+}
+
+variable "billing_info" {
+  type = object({
+    billing_export_table      = string
+    billing_export_email      = string
+    billing_export_project_id = string
+  })
+  description = "(*Optional*) The existing billing export information. Required for sub accounts."
+
+  default = {
+    billing_export_table      = ""
+    billing_export_email      = ""
+    billing_export_project_id = ""
+  }
 }
--- providers.tf
+++ providers.tf
@@ -1,8 +1,9 @@
 terraform {
+  required_version = ">= 1.9.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.0, <7.0"
+      version = ">= 5.0, < 8.0"
     }
   }
 }
